30b84c7269ad0a45f30bc0cec7fe3f7283ad570b
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-08-04 16:29:38 +0900

    indent-tabs-mode by setq-default

--- init-default/00-basic.el
+++ init-default/00-basic.el
@@ -39,16 +39,5 @@
 ; use y-n
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-; custom-set-variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(indent-tabs-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; Prevent Extraneous Tabs
+(setq-default indent-tabs-mode nil)
