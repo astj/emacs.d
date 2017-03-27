17e0c1ea9479b8d320931d0c3b7a8f92d5237ba3
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-03-27 12:34:14 +0900

    なんか見づらくなったので theme つける

--- init.el
+++ init.el
@@ -24,6 +24,9 @@
 (el-get-bundle! init-loader)
 (init-loader-load (locate-user-emacs-file "init-default"))
 
+; Use color scheme tando-dark
+(load-theme 'tango-dark t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
