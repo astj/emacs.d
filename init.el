9ce46e65dc95b344497d976fab4dbad81c70c0c5
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-03-27 12:23:44 +0900

    なんかついてた

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;refs http://d.hatena.ne.jp/tarao/20150221/1424518030
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
@@ -17,3 +24,16 @@
 (el-get-bundle! init-loader)
 (init-loader-load (locate-user-emacs-file "init-default"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (typescript-mode)))
+ '(ruby-insert-encoding-magic-comment nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
