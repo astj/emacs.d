dc9daee5d9bec356285416888d2bbf7511886a6e
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-12-05 14:12:05 +0900

    no more ensime

--- init-default/50-init-scala-mode.el
+++ init-default/50-init-scala-mode.el
@@ -1,3 +1,3 @@
 (el-get-bundle scala-mode :type git :url "git@github.com:ensime/emacs-scala-mode.git")
-(el-get-bundle! ensime :type git :url "git@github.com:ensime/ensime-emacs.git")
-(setq ensime-startup-snapshot-notification nil)
+;; (el-get-bundle! ensime :type git :url "git@github.com:ensime/ensime-emacs.git")
+;; (setq ensime-startup-snapshot-notification nil)
