e747c0bf8b4b6727033bdf2b59d3082dfadd7daf
Asato Wakisaka <asato.wakisaka@gmail.com>
2019-12-19 12:13:43 +0900

    2スペ

--- init.el
+++ init.el
@@ -33,7 +33,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (typescript-mode)))
- '(ruby-insert-encoding-magic-comment nil))
+ '(ruby-insert-encoding-magic-comment nil)
+ '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
