f74b263b881c5f258224161889e30f74314503d3
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-07-01 10:46:43 +0900

    高級 python モードやめる

--- init-default/59-init-python-mode.el
+++ init-default/59-init-python-mode.el
@@ -1,9 +1,9 @@
 ;; http://blog.shibayu36.org/entry/2017/04/02/193000
 
-(el-get-bundle! jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+;; (el-get-bundle! jedi)
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:complete-on-dot t)
 
-(el-get-bundle! virtualenvwrapper)
-(el-get-bundle! robert-zaremba/auto-virtualenvwrapper)
-(add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+;; (el-get-bundle! virtualenvwrapper)
+;; (el-get-bundle! robert-zaremba/auto-virtualenvwrapper)
+;; (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
