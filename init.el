1c94892525d2d0399ce920f1671c7db41d820320
Asato Wakisaka <asato.wakisaka@gmail.com>
2022-04-22 21:27:00 +0900

    I found something comitted

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (typescript-mode)))
+ '(package-selected-packages '(typescript-mode))
  '(ruby-insert-encoding-magic-comment nil)
  '(typescript-indent-level 2))
 (custom-set-faces
